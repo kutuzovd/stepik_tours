cfbf16d45e6ad56624aa34c31d248f0bd2f4ef0e
kutuzovd <kutuzov.ieee@gmail.com>
2020-03-21 20:13:17 +0400

    First project on Django

---
+++ stepik_tours/tours/views.py
@@ -0,0 +1,22 @@
+# Create your views here.
+from django.conf import settings
+from django.shortcuts import render
+from django.views import View
+
+
+class MainView(View):
+    def get(self, request, *args, **kwargs):
+        return render(
+            request, 'index.html')
+
+
+class DepartureView(View):
+    def get(self, request, *args, **kwargs):
+        return render(
+            request, 'departure.html')
+
+
+class TourView(View):
+    def get(self, request, *args, **kwargs):
+        return render(
+            request, 'tour.html')
