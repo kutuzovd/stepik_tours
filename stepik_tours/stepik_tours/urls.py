5a4b1ffdb19b8841574ff1aadd9fafc98c6dd86c
kutuzovd <kutuzov.ieee@gmail.com>
2020-03-21 22:12:31 +0400

    My progect 1.0

--- stepik_tours/stepik_tours/urls.py
+++ stepik_tours/stepik_tours/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from tours.views import MainView
-from tours.views import DepartureView
-from tours.views import TourView
+from tours.views import MainView, DepartureView, TourView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('', MainView.as_view(), name='main'),
     path('departure/<str:departure>', DepartureView.as_view(), name='departure'),
     path('tour/<int:id>/', TourView.as_view(), name='tour'),
